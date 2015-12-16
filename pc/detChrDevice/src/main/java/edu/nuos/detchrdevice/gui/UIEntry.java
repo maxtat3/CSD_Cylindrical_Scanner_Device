6a58cbfac2715351b356961756a146c3f1c14c02
Max <multi3volume@gmail.com>
2015-12-16 19:51:11 +0200

    В названиях подписей к столбцам при сохранении в файл запятые изменены на скобки т.к. например в libreoffice calc запятые являються разделителями включенными по умолчанию.

--- pc/detChrDevice/src/main/java/edu/nuos/detchrdevice/gui/UIEntry.java
+++ pc/detChrDevice/src/main/java/edu/nuos/detchrdevice/gui/UIEntry.java
@@ -41,7 +41,7 @@ public class UIEntry implements UART.CallbackADCData{
 	/**
 	 * Экземпляр класса для записи данных в csv файл .
 	 */
-	private Recorder recorder = new Recorder("point number", "Длина образца. мм", "Значение напряжения, В", 300);
+	private Recorder recorder = new Recorder("point number", "Длина образца (мм)", "Значение напряжения (В)", 300);
 
 
 	public UIEntry() {
